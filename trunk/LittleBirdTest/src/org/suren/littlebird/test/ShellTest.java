d8a7132e15e8479ec12624ee3744ebfc48dec6d5
zxjlwt@126.com <zxjlwt@126.com@e043543e-c475-5ce0-9c66-5f255cae42e2>
2013-12-09 01:10:00 +0000

    1、提交用于测试的项目； 2、删除bin目录，并 忽略提交

---
+++ trunk/LittleBirdTest/src/org/suren/littlebird/test/ShellTest.java
@@ -0,0 +1,31 @@
+package org.suren.littlebird.test;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.suren.littlebird.shell.Shell;
+
+public class ShellTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Shell shell = (Shell)Naming.lookup("rmi://10.0.32.3:/8989/Shell");
+			
+			String env = shell.getEnv("PATH");
+			
+			System.out.println(env);;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
