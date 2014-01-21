2d8508dfeee66fd455ef089ea25183f60218d7e2
zxjlwt@126.com <zxjlwt@126.com@e043543e-c475-5ce0-9c66-5f255cae42e2>
2014-01-21 01:51:02 +0000

    添加有关加密的面板

--- LittleBirdHome/src/org/suren/littlebird/gui/menu/HelpMenu.java
+++ LittleBirdHome/src/org/suren/littlebird/gui/menu/HelpMenu.java
@@ -2,7 +2,7 @@ package org.suren.littlebird.gui.menu;
 
 import org.suren.littlebird.annotation.Menu;
 
-@Menu(displayName = "Help", parentMenu = Object.class, index = 3)
+@Menu(displayName = "Help", parentMenu = Object.class, index = 4)
 public class HelpMenu extends ArchMenu
 {
 }
---
+++ LittleBirdHome/src/org/suren/littlebird/gui/menu/SecurityMenu.java
@@ -0,0 +1,9 @@
+package org.suren.littlebird.gui.menu;
+
+import org.suren.littlebird.annotation.Menu;
+
+@Menu(displayName = "Security", parentMenu = Object.class, index = 2)
+public class SecurityMenu extends ArchMenu
+{
+
+}
