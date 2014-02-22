acbfc2ffd2571843f9cc27b8d5b225622d686b7b
zxjlwt@126.com <zxjlwt@126.com@e043543e-c475-5ce0-9c66-5f255cae42e2>
2014-02-22 09:35:40 +0000

    完成基本的bundle管理功能

--- suren.bundle.manager/src/org/suren/littlebird/server/BundleServer.java
+++ suren.bundle.manager/src/org/suren/littlebird/server/BundleServer.java
@@ -3,16 +3,18 @@ package org.suren.littlebird.server;
 import java.io.Serializable;
 import java.util.List;
 
-
 public interface BundleServer extends Serializable
 {
-	public String hello();
-
 	public List<SuRenBundle> getAll();
+	public List<SuRenBundle> searchBy(String match);
+	public List<SuRenBundle> matchBy(String regex);
+	public List<SuRenBundle> matchBy(String regex, boolean insensitive);
+	public List<SuRenBundle> matchBy(String regex, int flag);
 	public SuRenBundle getById(long id);
 
 	public int start(long ... ids);
 	public int stop(long ... ids);
+	public int update(long ... ids);
 
 	public int install(String ... paths);
 	public int uninstall(long ... ids);
