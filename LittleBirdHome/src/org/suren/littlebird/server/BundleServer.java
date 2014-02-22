a0074f3addb51e72fcbcd2709c39fa176a0fc198
zxjlwt@126.com <zxjlwt@126.com@e043543e-c475-5ce0-9c66-5f255cae42e2>
2014-02-22 09:34:49 +0000

    完善osgi管理功能

--- LittleBirdHome/src/org/suren/littlebird/server/BundleServer.java
+++ LittleBirdHome/src/org/suren/littlebird/server/BundleServer.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 public interface BundleServer extends Serializable
 {
-	public String hello();
-
 	public List<SuRenBundle> getAll();
+	public List<SuRenBundle> searchBy(String match);
+	public List<SuRenBundle> matchBy(String regex);
+	public List<SuRenBundle> matchBy(String regex, boolean insensitive);
+	public List<SuRenBundle> matchBy(String regex, int flag);
 	public SuRenBundle getById(long id);
 
 	public int start(long ... ids);
 	public int stop(long ... ids);
+	public int update(long ... ids);
 
 	public int install(String ... paths);
 	public int uninstall(long ... ids);
