de8f922367669e1ea713d059fc19371715cc004d
zxjlwt@126.com <zxjlwt@126.com@e043543e-c475-5ce0-9c66-5f255cae42e2>
2014-02-24 10:18:24 +0000

    完善osgi管理界面

---
+++ LittleBirdHome/src/org/suren/littlebird/server/BundleHeader.java
@@ -0,0 +1,29 @@
+package org.suren.littlebird.server;
+
+public class BundleHeader
+{
+	private Object key;
+	private Object value;
+
+	public Object getKey()
+	{
+		return key;
+	}
+	public void setKey(Object key)
+	{
+		this.key = key;
+	}
+	public Object getValue()
+	{
+		return value;
+	}
+	public void setValue(Object value)
+	{
+		this.value = value;
+	}
+	@Override
+	public String toString()
+	{
+		return "[key=" + key + ", value=" + value + "]";
+	}
+}
